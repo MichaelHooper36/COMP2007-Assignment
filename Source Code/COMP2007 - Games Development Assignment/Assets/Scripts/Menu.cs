35139ad7645132a6c0857cf42718ab0b27b6c958
Michael Hooper <mikeyhooper360@gmail.com>
2025-05-03 22:41:35 +0100

    letter creation and more menus

--- Source Code/COMP2007 - Games Development Assignment/Assets/Scripts/Menu.cs
+++ Source Code/COMP2007 - Games Development Assignment/Assets/Scripts/Menu.cs
@@ -14,24 +14,43 @@ public class Menu : MonoBehaviour
     public GameObject main_menu;
     public GameObject solve_menu;
     public GameObject settings_menu;
+    public GameObject tutorial_menu;
+    public GameObject confirm_menu;
+    public GameObject verdict_menu;
 
     public GameObject menu_reminder;
     public GameObject current_suspect;
+    public GameObject suspect_text;
 
+    public static bool tutorial_on = true;
+    public static bool menu_on = false;
     public static bool gun_obtained = false;
     public static bool knife_obtained = false;
     public static int ending = 0;
+
+    public string final_verdict = "";
     // Start is called before the first frame update
     void Start()
     {
-        
+        FPSController.canMove = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        tutorial_menu.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (tutorial_on && Input.GetKey(KeyCode.Return))
         {
+            tutorial_on = false;
+            tutorial_menu.SetActive(false);
+            FPSController.canMove = true;
+        }
+
+        if (!menu_on && Input.GetKey(KeyCode.Escape))
+        {
+            menu_on = true;
             FPSController.canMove = false;
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
@@ -50,6 +69,7 @@ public class Menu : MonoBehaviour
     {
         if (main_menu != null)
         {
+            menu_on = false;
             main_menu.SetActive(false);
             menu_reminder.SetActive(true);
             Cursor.lockState = CursorLockMode.Locked;
@@ -101,13 +121,17 @@ public class Menu : MonoBehaviour
         }
     }
 
-    public void ConfirmSuspicion()
+    public void AccuseSuspect()
     {
-
+        confirm_menu.SetActive(true);
     }
 
     public void BackFromSuspect()
     {
+        if (confirm_menu.activeInHierarchy)
+        {
+            confirm_menu.SetActive(false);
+        }
         solve_menu.SetActive(false);
         main_menu.SetActive(true);
     }
@@ -116,5 +140,35 @@ public class Menu : MonoBehaviour
     {
         settings_menu.SetActive(false);
         main_menu.SetActive(true);
+    }
+
+    public void ConfirmButton()
+    {
+        switch(ending)
+        {
+            case 1:
+                final_verdict = "Ryan Gosling was not the killer";
+                break;
+            case 2:
+                final_verdict = "Jack D. Ripper was not the killer";
+                break;
+            case 3:
+                final_verdict = "Harry Houdini was the killer";
+                break;
+            case 4:
+                final_verdict = "You were not the killer";
+                break;
+        }
+
+        suspect_text.GetComponent<TextMeshProUGUI>().text = final_verdict;
+
+        confirm_menu.SetActive(false);
+        solve_menu.SetActive(false);
+        verdict_menu.SetActive(true);
+    }
+
+    public void CancelButton()
+    {
+        confirm_menu.SetActive(false);
     }
 }
